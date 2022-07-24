8670795ffda49caa8fffb887d936d4a4898bc223
Jajnaseni <jajnaseni.majhi@gmail.com>
2022-07-24 11:38:45 +0530

    Special Median problem

--- Heaps/SpecialMedian.cs
+++ Heaps/SpecialMedian.cs
@@ -6,114 +6,167 @@ using System.Threading.Tasks;
 
 namespace Heaps
 {
-    public class SpecialMedian
+    class Solution
     {
-        
         public int solve(List<int> A)
         {
-            Heap leftMaxHeap = new Heap(0);
-            Heap rightMinHeap = new Heap(0);
-            int size = A.Count;
-            leftMaxHeap.InserttoMaxHeap(A[0]);
-            int currMedian = A[0];
-            //Check for left side
+            int medianLeft = CheckSpecialMedian(A);
+            A.Reverse();
+            int medianRight = CheckSpecialMedian(A);
+            if (medianLeft == -1 && medianRight == -1)
+            {
+                return 0;
+            }
+            return 1;
+        }
+        private int CheckSpecialMedian(List<int> A)
+        {
+            decimal currentMedian = A[0];
+            List<int> maxHeap = new List<int>();
+            List<int> minHeap = new List<int>();
+            maxHeap.Add(A[0]);
             for (int i = 1; i < A.Count; i++)
             {
-               
-                if (A[i]==currMedian)
+                if (A[i] == currentMedian)
                 {
                     return 1;
                 }
-                if (A[i]>currMedian)
+                if (A[i] > maxHeap[0])
                 {
-                    rightMinHeap.InserttoMinHeap(A[i]);
+                    InsertInMinHeapOneByOne(minHeap, A[i]);
                 }
                 else
                 {
-                    leftMaxHeap.InserttoMaxHeap(A[i]);
+                    InsertInMaxHeapOneByOne(maxHeap, A[i]);
                 }
-
-                if(rightMinHeap.heapList.Count>leftMaxHeap.heapList.Count)
+                if (maxHeap.Count - minHeap.Count > 1)
                 {
-                  int value=  rightMinHeap.getMinimumFromMinHeap();
-                  leftMaxHeap.InserttoMaxHeap(value);
+                    int elem = RemoveFromHeap(maxHeap);
+                    InsertInMinHeapOneByOne(minHeap, elem);
                 }
-                else if ((leftMaxHeap.heapList.Count-rightMinHeap.heapList.Count)>1)
+                else if (minHeap.Count > maxHeap.Count)
                 {
-                    rightMinHeap.InserttoMinHeap(leftMaxHeap.getMaximumfromMaxHeap());
+                    int elem = RemoveFromMinHeap(minHeap);
+                    InsertInMaxHeapOneByOne(maxHeap, elem);
                 }
-
-                if(leftMaxHeap.heapList.Count==rightMinHeap.heapList.Count)
+                if ((i + 1) % 2 == 0)
                 {
-                    int leftMax = leftMaxHeap.getMaximumfromMaxHeap();
-                    int rightMin = rightMinHeap.getMinimumFromMinHeap();
-                    currMedian = (leftMax + rightMin) / 2;
-
-                    leftMaxHeap.InserttoMaxHeap(leftMax);
-                    rightMinHeap.InserttoMinHeap(rightMin);
+                    currentMedian = maxHeap[0] + ((minHeap[0] - (decimal)maxHeap[0]) / 2);
                 }
                 else
                 {
-                    currMedian = leftMaxHeap.getMaximumfromMaxHeap();
-                    leftMaxHeap.InserttoMaxHeap(currMedian);
+                    currentMedian = maxHeap[0];
                 }
-                //if (currMedian == A[i])
-                //{
-                //    return 1;
-                //}
-
             }
-
-           
-           //Check for right side
-            leftMaxHeap= new Heap(0);
-            rightMinHeap = new Heap(0);
-            leftMaxHeap.InserttoMaxHeap(A[size - 1]);
-            currMedian = A[size - 1];
-            for (int i = size-2; i>=0; i--)
+            return -1;
+        }
+        private int RemoveFromHeap(List<int> arr)
+        {
+            int result = 0;
+            if (arr.Count == 0)
             {
-                
-                if (A[i] == currMedian && currMedian !=0)
+                return -1;
+            }
+            result = arr[0];
+            Swap(arr, 0, arr.Count - 1);
+            arr.RemoveAt(arr.Count - 1);
+            int parentIndex = 0;
+            int childOneIndex = parentIndex * 2 + 1;
+            int childTwoIndex = parentIndex * 2 + 2;
+            int greaterValueIndex = 0;
+            if (childOneIndex >= arr.Count && childTwoIndex >= arr.Count)
+            {
+                greaterValueIndex = -1;
+            }
+            else
+            {
+                greaterValueIndex = childTwoIndex < arr.Count ? arr[childTwoIndex] > arr[childOneIndex] ? childTwoIndex : childOneIndex : childOneIndex;
+            }
+            while (greaterValueIndex != -1 && arr[parentIndex] < arr[greaterValueIndex])
+            {
+                Swap(arr, parentIndex, greaterValueIndex);
+                parentIndex = greaterValueIndex;
+                childOneIndex = parentIndex * 2 + 1;
+                childTwoIndex = parentIndex * 2 + 2;
+                if (childOneIndex >= arr.Count && childTwoIndex >= arr.Count)
                 {
-                    return 1;
-                }
-                if (A[i] > currMedian)
-                {
-                    rightMinHeap.InserttoMinHeap(A[i]);
-                }
-                else
-                {
-                    leftMaxHeap.InserttoMaxHeap(A[i]);
-                }
-
-                if (rightMinHeap.heapList.Count > leftMaxHeap.heapList.Count)
-                {
-                    int value = rightMinHeap.getMinimumFromMinHeap();
-                    leftMaxHeap.InserttoMaxHeap(value);
-                }
-                else if ((leftMaxHeap.heapList.Count - rightMinHeap.heapList.Count) > 1)
-                {
-                    rightMinHeap.InserttoMinHeap(leftMaxHeap.getMaximumfromMaxHeap());
-                }
-
-                if (leftMaxHeap.heapList.Count == rightMinHeap.heapList.Count)
-                {
-                    int leftMax = leftMaxHeap.getMaximumfromMaxHeap();
-                    int rightMin = rightMinHeap.getMinimumFromMinHeap();
-                    currMedian = (leftMax + rightMin) / 2;
-
-                    leftMaxHeap.InserttoMaxHeap(leftMax);
-                    rightMinHeap.InserttoMinHeap(rightMin);
+                    greaterValueIndex = -1;
                 }
                 else
                 {
-                    currMedian = leftMaxHeap.getMaximumfromMaxHeap();
-                    leftMaxHeap.InserttoMaxHeap(currMedian);
+                    greaterValueIndex = childTwoIndex < arr.Count ? arr[childTwoIndex] > arr[childOneIndex] ? childTwoIndex : childOneIndex : childOneIndex;
                 }
             }
-
-
-            return 0;
+            return result;
+        }
+        private int RemoveFromMinHeap(List<int> arr)
+        {
+            int result = 0;
+            if (arr.Count == 0)
+            {
+                return -1;
+            }
+            result = arr[0];
+            Swap(arr, 0, arr.Count - 1);
+            arr.RemoveAt(arr.Count - 1);
+            int parentIndex = 0;
+            int childOneIndex = parentIndex * 2 + 1;
+            int childTwoIndex = parentIndex * 2 + 2;
+            int greaterValueIndex = 0;
+            if (childOneIndex >= arr.Count && childTwoIndex >= arr.Count)
+            {
+                greaterValueIndex = -1;
+            }
+            else
+            {
+                greaterValueIndex = childTwoIndex < arr.Count ? arr[childTwoIndex] < arr[childOneIndex] ? childTwoIndex : childOneIndex : childOneIndex;
+            }
+            while (greaterValueIndex != -1 && arr[parentIndex] > arr[greaterValueIndex])
+            {
+                Swap(arr, parentIndex, greaterValueIndex);
+                parentIndex = greaterValueIndex;
+                childOneIndex = parentIndex * 2 + 1;
+                childTwoIndex = parentIndex * 2 + 2;
+                if (childOneIndex >= arr.Count && childTwoIndex >= arr.Count)
+                {
+                    greaterValueIndex = -1;
+                }
+                else
+                {
+                    greaterValueIndex = childTwoIndex < arr.Count ? arr[childTwoIndex] < arr[childOneIndex] ? childTwoIndex : childOneIndex : childOneIndex;
+                }
+            }
+            return result;
+        }
+        private void InsertInMinHeapOneByOne(List<int> arr, int elem)
+        {
+            arr.Add(elem);
+            int insertedIndex = arr.Count - 1;
+            int parentIndex = (insertedIndex - 1) / 2;
+            while (insertedIndex != 0 && arr[parentIndex] > arr[insertedIndex])
+            {
+                Swap(arr, parentIndex, insertedIndex);
+                insertedIndex = parentIndex;
+                parentIndex = (insertedIndex - 1) / 2;
+            }
+        }
+        private void InsertInMaxHeapOneByOne(List<int> arr, int elem)
+        {
+            arr.Add(elem);
+            int insertedIndex = arr.Count - 1;
+            int parentIndex = (insertedIndex - 1) / 2;
+            while (insertedIndex != 0 && arr[parentIndex] < arr[insertedIndex])
+            {
+                Swap(arr, parentIndex, insertedIndex);
+                insertedIndex = parentIndex;
+                parentIndex = (insertedIndex - 1) / 2;
+            }
+        }
+        private void Swap(List<int> arr, int index1, int index2)
+        {
+            int temp = arr[index1];
+            arr[index1] = arr[index2];
+            arr[index2] = temp;
         }
     }
 }
