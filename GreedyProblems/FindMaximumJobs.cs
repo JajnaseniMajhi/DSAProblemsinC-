3d8f02f4ea1fb3024c8d852bd069dac495bb2740
Jajnaseni <jajnaseni.majhi@gmail.com>
2022-07-24 21:31:59 +0530

    Find maximum jobs

---
+++ GreedyProblems/FindMaximumJobs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreedyProblems
+{
+    public class FindMaximumJobs
+    {
+        public int Solve(List<int> A, List<int> B)
+        {
+            List<KeyValuePair<int, int>> mapping = new List<KeyValuePair<int, int>>();
+
+            for (int i=0;i<A.Count;i++)
+            {
+                mapping.Add(new KeyValuePair<int, int>(A[i], B[i]));
+            }
+
+            mapping.Sort((x, y) => x.Value.CompareTo(y.Value));
+            int count = 0;
+            int endTime = int.MinValue;
+            for(int i=0;i<A.Count;i++)
+            {
+                if (mapping[i].Key >=endTime)
+                {
+                    count = count + 1;
+                    endTime = mapping[i].Value;
+                }
+            }
+
+            Console.WriteLine(count);
+            foreach(var x in mapping)
+            {
+                Console.Write(x.Key + " ");
+                Console.Write(x.Value);
+                Console.WriteLine();
+            }
+
+            return 0;
+        }
+    }
+}
--- GreedyProblems/Program.cs
+++ GreedyProblems/Program.cs
@@ -10,8 +10,8 @@ namespace GreedyProblems
     {
         static void Main(string[] args)
         {
-            FreeCars freeCars = new FreeCars();
-            freeCars.Solve(new List<int>() { 1, 7, 6, 2, 8, 4, 4, 6, 8, 2 }, new List<int>() { 8, 11, 7, 7, 10, 8, 7, 5, 4, 9 });
+            FindMaximumJobs freeCars = new FindMaximumJobs();
+            freeCars.Solve(new List<int>() { 3, 2, 6 }, new List<int>() { 9, 8, 9 });
             Console.ReadLine();
         }
     }
